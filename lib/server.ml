0094c297fa09f342617a9eb7f3bd307da35f3a16
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-23 22:34:31 -0300

    opium server

--- lib/server.ml
+++ lib/server.ml
@@ -1,6 +1,16 @@
 open Opium
 
+type message = {
+  id: Uuidm.t;
+  message: string;
+} [@@deriving yojson]
+
+let get_message req = req
+
+let post_message req = req'
+
 let init_server = 
   App.empty 
-  |> App.port 4000
+  |> App.get "/message/:id" get_message
+  |> App.post "/message" post_message
   |> App.run_multicore
