b546877a46693efd347998195172b38c4549a054
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-24 22:14:25 -0300

    create table

--- lib/server.ml
+++ lib/server.ml
@@ -5,6 +5,25 @@ type message = {
   message: string;
 } [@@deriving yojson]
 
+let connection_pg_url = "postgresql://postgres:password@localhost:5432"
+
+exception Query_Error of string
+
+let ( let* ) = Lwt.bind
+
+let pool = 
+  match Caqti_lwt.connect_pool ~max_size: 10 (Uri.of_string connection_pg_url) with
+  | Ok pool -> pool
+  | Error error -> failwith (Caqti_error.show error)
+
+let dispatch f =
+  let* result = Caqti_lwt.Pool.use f pool in
+  match result with
+  | Ok data -> Lwt.return data
+  | Error error -> Lwt.fail (Query_Error (Caqti_error.show error))
+
+let () = dispatch (Queries.ensure_table_exists ()) |> Lwt_main.run
+
 let get_message _req = 
   Response.of_plain_text "Hello World" |> Lwt.return
 
